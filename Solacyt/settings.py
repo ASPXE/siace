b34a722bc39081997da664027a69f204b1934d68
aspxe <aspxelite@outlook.com>
2024-03-18 16:19:40 -0600

    Se han creado las aplicaciones de administradores, eventos y correos.

--- Solacyt/settings.py
+++ Solacyt/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'core'
+    'core',
+    'administradores',
+    'eventos',
+    'correos'
 ]
 
 MIDDLEWARE = [
