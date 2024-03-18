149d20919ad9f124e9e6118e382caba53fa2ee33
aspxe <aspxelite@outlook.com>
2024-03-18 16:40:36 -0600

    Los modelos de la aplicación de Eventos han sido creados. Se crearon los modelos Categorias, Eventos, Participantes, Asistencias, Qrs y QrsParticipantes

--- administradores/models.py
+++ administradores/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Administradores(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    administrador = models.CharField(max_length=45)
+    nombreAdministrador = models.CharField(max_length=30, null=False, blank=False)
+    contrasenia = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        db_table = "Adimistradores"
--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+class Categorias(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+
+    class Meta:
+        db_table = "Categorias"
+
+class Eventos(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=70)
+    fechaInicio = models.DateField(auto_now=False, auto_now_add=False, auto_created=False, null=False)
+    fechaFin = models.DateField(auto_now=False, auto_now_add=False, auto_created=False, null=False)
+    cantidadAsistentesMaximo = models.IntegerField(null=False)
+    estatus = models.BooleanField(null=True, default=False)
+    categoria = models.ForeignKey(Categorias, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "Eventos"
+
+class Participantes(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=45, null=False, blank=False)
+    apellidoPaterno = models.CharField(max_length=45, null=False, blank=False)
+    apellidoMaterno = models.CharField(max_length=45, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    evento = models.ForeignKey(Eventos, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "Participantes"
+
+class Asistencias(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    fechaEntrada = models.DateTimeField(auto_now=False, auto_now_add=False, auto_created=False, null=False)
+    fechaSalida = models.DateTimeField(auto_now=False, auto_now_add=False, auto_created=False, null=True)
+    participante = models.ForeignKey(Participantes, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "Asistencias"
+
+class Qrs(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    # Hay que definir un formato de imágen específico en este campo
+    imagen = models.ImageField()
+    datos = models.CharField(max_length=100, null=False)
+    estatus = models.BooleanField(null=False, default=True)
+    evento = models.ForeignKey(Eventos, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "Qrs"
+
+class QrsParticipantes(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    qr = models.ForeignKey(Qrs, on_delete=models.PROTECT)
+    participante = models.ForeignKey(Participantes, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "QrsParticipantes"
